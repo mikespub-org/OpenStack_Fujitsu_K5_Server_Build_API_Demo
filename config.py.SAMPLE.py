0696272446ed791aad5a00f3cc8cd0d604ad51b0
joergK5Schulz <joerg.schulz@ts.fujitsu.com>
2017-03-28 17:35:54 +0200

    change server info to directory, same to availability zone.

--- config.py.SAMPLE.py
+++ config.py.SAMPLE.py
@@ -24,33 +24,55 @@ region='uk-1' # or any other region
 projectid='YourK5ProjectID' # might be obsolete
 securityGroup = 'YourSecGroupMust Exist'
 # securityGroupID = '11113222222333333'
-networkName = 'yourDesiredNetworkName'
+
 subnetName='yourDesiredSubnetName'
 subnetAddress='10.10.0.0/16' # pick it along your design
 allocationPoolStart= '10.10.10.1' # pick it along your design
 allocationPoolEnd='10.10.100.255' # pick it along your design
-routerName = 'YourRouterName'
-defaultRoute='10.10.0.1'    # pick it along your design, see net config above
-
-externalNet = 'inf_az1_ext-net01'   # recommended one for uk-1
-nameserver1 = '62.60.39.9'  # recommended one for uk-1, see documentation
-# nameserver2 = '62.60.39.10'
-nameserver2 = '8.8.8.8' # Google, just to have a safe bet
-
+"""
+basics for the availability zones
+"""
+availabilityZone = 'uk-1a'
+# availabilityZone = 'uk-1b'
+zoneInfo = { 'uk-1a' : {'externalNet'   : 'inf_az1_ext-net01',
+                        'nameserver1'   : '62.60.42.9' ,
+                        'nameserver2'   : '8.8.8.8',
+                        'networkName'   : 'oceania-1a',
+                        'routerName'    : 'oceania01-a'},
+             'uk-1b' : {'externalNet'   : 'inf_az2_ext-net01',
+                        'nameserver1'   : '62.60.39.9' ,
+                        'nameserver2'   : '8.8.8.8',
+                        'networkName'   : 'oceania-1b',
+                        'routerName'    : 'oceania01-b'}
+             }
 
 
 key='NameOfecretKeyPair'
 
+# this didn't work yet. Would immediately update Ubuntu14 to 16.
+initialLxUpgrade=""" #!/bin/bash
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo do-release-upgrade -f DistUpgradeViewNonInteractive
+"""
+
 # server parameters; 
+ubuntuServerInfo = {
+    'name' : 'Oceania02LX',
+    'imageRef' : '45895e58-e416-4b96-9576-1df6cba6e264',
+    'flavorid' : '1902',
+    'volumesize' : '20',
+    'availabilityZone' : availabilityZone,
+    'networkPortName' : networkPortName,
+    'security_group_name' : securityGroup,
+    'sshkey_name' : key,
+    'initialPassword' : False,
+    'initialScript' : initialLxUpgrade
+}
+
 if testing:
-    serverName='yourServerName'
-    imageRef="6e1610db-1115-4260-8dc2-bcdd526a54be" # grab it from the sample output of fjk5.py. This one is a Ubuntu*14*LTS
-    flavor='1302'   # pretty small footprint. Refer to docs.
-    volumesize = '90' # take what you need, must be bigger than minsize for selected image.
-    availabilityZone = 'uk-1a'  # if you chose a different one, you have to chose a different external net above
-    networkPortName = 'SomePortName'
-    initialPassword = 'WindowsPassword' # CHANGE THIS ONE AS SOON AS YOU HAVE ACCESS TO THE SERVER. Only for Windows.
-
-
+    # serverInfo = ubuntuServerInfo
+    serverInfo = windowsServerInfo
+    
 
 
